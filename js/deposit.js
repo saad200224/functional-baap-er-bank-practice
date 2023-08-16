05efccefd7ee132825bfcda62209b5ad81cecf7d
saad200224 <saad19021994@outlook.com>
2023-08-16 16:48:36 +0600

    hi

---
+++ js/deposit.js
@@ -0,0 +1,15 @@
+document.getElementById('btn-deposit').addEventListener('click', function(){
+
+    const newDepositAmount = getInputFieldValueById('deposit-field');
+
+    const previousDepositTotal = getTextElementValueById('deposit-total');
+
+    const newDepositTotal = newDepositAmount + previousDepositTotal;
+
+    setTextElementValueById('deposit-total', newDepositTotal);
+
+    const previousBalanceTotal = getTextElementValueById('balance-total');
+    const newBalanceTotal = previousBalanceTotal + newDepositAmount; 
+
+    setTextElementValueById('balance-total', newBalanceTotal);
+})
